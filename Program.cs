884cfb48fa978eb6886499a3121ede215853afd8
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-24 16:40:52 -0300

    finalização

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cidades_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhahém");
+            Console.WriteLine("Peruíbe");
+            
+
+        }
+    }
+}
